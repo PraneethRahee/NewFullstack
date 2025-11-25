1d2b82650cf0d10ab5d8271afa90879be5d68983
Praneeth Rahee <raheepraneeth69@gmail.com>
2025-11-25 10:51:02 +0530

    Initial

---
+++ components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+const Header = () => {
+  return (
+    <>
+      <nav className='fixed top-0 left-0 right-0 z-20 bg-background/80 backdrop-blur-xl border-b '>
+        <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
+          <Link href="/" className='flex items-center'>
+            <Image src="/logo.svg" alt="logo" width={500} height={500} className='h-11' priority/>
+          </Link>
+          
+        </div>
+
+      </nav>
+    </>
+  )
+}
+
+export default Header
